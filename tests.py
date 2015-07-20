66d9951a7ede1d8e2f81f98042c0bb421fd63c7f
Dome knoxz Molitor <promolitor@gmail.com>
2015-07-20 20:19:12 +0200

    not important

--- tests.py
+++ tests.py
@@ -19,15 +19,10 @@ class TestRefParser(unittest.TestCase):
             return myfile.read()
 
     def _equal(self, txt, html):
-        f = open("raum1171.txt", "w")
-        text = Schedule(self.get_file(html)).__str__().encode("utf-8").strip()
-        print(text)
-        f.write(str(text))
-        f.close()
         self.assertEqual(self.get_file(txt).strip(), Schedule(self.get_file(html)).__str__().encode("utf-8").strip())
 
     def test_parse_schedule(self):
-        self._equal("raum5710.txt", "raum1171.html")
+        self._equal("raum1171_richtig.txt", "raum1171.html")
         #self._equal("raum5710_2.txt", "raum5710_2.html")
 
 if __name__ == '__main__':
